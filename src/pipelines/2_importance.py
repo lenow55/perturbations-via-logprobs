0b33d5f2262b193fe46a36f411d9bd5d6a699f79
Новиков Илья <nov.ilucha@yandex.ru>
2026-03-09 10:28:49 +0300

    reading for stage2

--- src/pipelines/2_importance.py
+++ src/pipelines/2_importance.py
@@ -1,25 +1,31 @@
 import asyncio
 import csv
-import json
 import logging
 import os
 from argparse import Namespace
+from datetime import datetime
 
 import pandas as pd
 from openai import AsyncOpenAI
+from pydantic import BaseModel
 
-from src.config import AppSettings, ChatLLMConfig, EmbedLLMConfig
+from src.config import AppSettings
 from src.params import parser
-from src.schemas import CheckLlmIn, CheckStage1Out
+from src.schemas import CheckLlmIn
 from src.utils.base import (
-    calculate_prompt_logprobs,
     configure_logging,
 )
+from src.utils.metrics_hub import HUB
 
 logger = logging.getLogger(__name__)
 
-# INFO: декодируем сразу весь запрос и ответ эталонный
-# для получения логпробов на каждый токен
+
+class MetadataFileInfo(BaseModel):
+    date: datetime = datetime.now()
+    input_folder: str
+    stage_folder: str
+    output_folder: str
+    metric_name: str
 
 
 def stage_task(
@@ -42,32 +48,54 @@ def main(args: Namespace):
         raise RuntimeError("Bad argument for report value")
     if not isinstance(args.config, str):
         raise RuntimeError("Bad argument for config path value")
+    if not isinstance(args.metric, str):
+        raise RuntimeError("Bad metric value")
+    if args.metric not in HUB:
+        raise RuntimeError(f"Metric '{args.metric}' not in HUB")
 
     with open(args.config, "r") as f:
         config = AppSettings.model_validate_json(f.read())
 
     configure_logging(config=config)
 
-    in_passages = os.path.join(args.input, "passages.json")
-    in_checks = os.path.join(args.input, "checks.csv")
-    with open(in_passages, "r") as f:
-        passages = json.load(f)
-    logger.info(f"Readed {in_passages} passages {len(passages)}")
-    checks_df = pd.read_csv(in_checks, quoting=csv.QUOTE_NONNUMERIC)
+    meta_file = os.path.join(args.out_folder, "meta.json")
+    if not os.path.isdir(args.out_folder):
+        logger.warning(f"Try create out_folder {args.out_folder}")
+        os.mkdir(args.out_folder)
+    else:
+        if os.path.exists(meta_file):
+            logger.error(f"Metadata file {meta_file} exists in target dir: ABORT!!!")
+            return
+        else:
+            logger.info("Empty dir already exists")
+
+    in_checks = os.path.join(args.stage, "checks.csv")
+    in_logprobs = os.path.join(args.stage, "gen_logprobs.csv")
+
+    # датасет с колонками
+    # ["question", "answer", "passage_id", "similarity"]
+    # индекс "check_id"
+    checks_df = pd.read_csv(in_checks, quoting=csv.QUOTE_NONNUMERIC, index_col=0)
     logger.info(f"Checks readed from {in_checks} shape: {checks_df.shape}")
 
-    client = create_openai_client(config=config.llm)
-    if not isinstance(config.embed, ChatLLMConfig):
-        logger.critical("Cant run without embed config")
-        return
+    # датасет с колонками
+    # ["prompt_logprobs", "gen_answer"]
+    # индекс "check_id"
+    logprobs_df = pd.read_csv(in_logprobs, quoting=csv.QUOTE_NONNUMERIC, index_col=0)
+    logger.info(
+        f"Logprobs dataset readed from {in_logprobs} shape: {logprobs_df.shape}"
+    )
 
-    client_embed = create_openai_client(config=config.embed)
-    semaphore = asyncio.Semaphore(config.llm.async_cals)
+    # INFO: сохранение
 
-    tasks: list[asyncio.Task[CheckStage1Out]] = []
-    for idx, row in checks_df.iterrows():
-        if not isinstance(idx, int):
-            raise RuntimeError("Bad index type")
+    meta_info = MetadataFileInfo(
+        input_folder=args.input,
+        stage_folder=args.stage,
+        output_folder=args.output,
+        metric_name=args.metric,
+    )
+    with open(meta_file, "w") as f:
+        _ = f.write(meta_info.model_dump_json(indent=2))
 
     df = pd.DataFrame.from_records(data=results, index="check_id")
     df.to_csv(args.output, quoting=csv.QUOTE_NONNUMERIC)
@@ -95,5 +123,12 @@ if __name__ == "__main__":
         type=str,
         required=True,
         help="Путь до файла с результатом подсчёта значимости",
+    )
+    _ = parser.add_argument(
+        "-m",
+        "--metric",
+        type=str,
+        required=True,
+        help="Код функции подсчёта метрики",
     )
     main(parser.parse_args())
